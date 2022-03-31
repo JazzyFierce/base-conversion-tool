f0f91e1b91a53ef561c547ca1e58a776e21fba8b
Isabel Loney <102490072+JazzyFierce@users.noreply.github.com>
2022-03-30 22:01:53 -0500

    Update based.py

--- based.py
+++ based.py
@@ -1,18 +1,18 @@
 import math
 import string
-import copy
 
 
 class ConversionFunctions:
     def __init__(self):
+        # a set of available characters
         self.alpha_num = list(string.digits) + list(string.ascii_uppercase)
 
     def dec_to_any(self, dec_num, base):
+        # the process for converting integers and fractional components is different, so we immediately separate them
         int_part = int(dec_num)
         float_part = dec_num % 1
         converted_int = []
         converted_float = []
-        alpha_num = copy.deepcopy(self.alpha_num)
 
         while int_part > 0:
             converted_int.insert(0, int_part % base)
@@ -23,48 +23,39 @@ class ConversionFunctions:
             converted_float.append(int(conversion_step))
             float_part = conversion_step - (int(conversion_step))
 
-        # converted numbers will be precise to 15 decimal points, rounded based on the 16th one
-        # this isn't ideal but the program will go into a death loop otherwise
-        # can you call them DECIMAL points if it's not in decimal base? Hmm...
+        # converted numbers will be precise to 15 digits for the fractional component
+        # rounding will be based on the ceiling of base/2
+        # i.e. anything larger than a half will be rounded up, which is pretty intuitive
+        try:
+            temp = converted_float[15]
+
+            if temp >= math.ceil(base / 2):
+                converted_float[14] += 1
+            converted_float.pop(15)
+        except IndexError:
+            # this basically means 15 digits of precision isn't necessary; in that case, the exact number is printed
+            pass
 
         if base <= 36:
-            try:
-                temp = converted_float[15]
-
-                if temp >= math.ceil(base / 2):  # 0 1 2 3 4 5 6 7 8 9
-                    converted_float[14] = int(converted_float[14] + 1)
-                converted_float.pop(15)
-            except IndexError:
-                pass
-
             for num in range(0, len(converted_int)):
                 if converted_int[num] > 9:
-                    converted_int[num] = alpha_num[converted_int[num]]
+                    converted_int[num] = self.alpha_num[converted_int[num]]
 
             for num in range(0, len(converted_float)):
                 if converted_float[num] > 9:
-                    converted_float[num] = alpha_num[converted_float[num]]
+                    converted_float[num] = self.alpha_num[converted_float[num]]
 
             if len(converted_float):
                 converted_int.append('.')
             return "".join([str(x) for x in converted_int + converted_float])
         else:
-            try:
-                temp = converted_float[15]
-                if temp >= math.floor(base/2):
-                    converted_float[14] += 1
-                converted_float.pop(15)
-            except IndexError:
-                pass
-
             if len(converted_float):
                 converted_int.append(" . ")
             return " ".join([str(x) for x in converted_int + converted_float])
 
     def any_to_dec_v1(self, base, num):  # converts to decimal with digits past 9 notated with letters
         digits = list(num)
-        alpha_num = copy.deepcopy(self.alpha_num)
-        available_chars = alpha_num[:base]
+        available_chars = self.alpha_num[:base]
 
         for digit in digits:
             if digit.upper() not in available_chars:
@@ -72,9 +63,9 @@ class ConversionFunctions:
 
         for index in range(0, len(digits)):
             if digits[index] in string.ascii_letters:
-                digits[index] = alpha_num.index(digits[index].upper())
+                digits[index] = self.alpha_num.index(digits[index].upper())
             else:
-                digits[index] = alpha_num.index(digits[index])
+                digits[index] = self.alpha_num.index(digits[index])
         digits.reverse()
         dec_num = 0
         for num in range(0, len(digits)):
